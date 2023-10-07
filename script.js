790855812a4e91b1fa5d36c36353a2b87acdb39d
Quebvihn <bamedinatorres@gmail.com>
2023-10-07 09:02:32 -0400

    added some base variables

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 var key = "5ac110fde26370792fef708e7e50de6d"
+var cities = []
+var api = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
