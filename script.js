20d57258ce4fab4c967971ead8f29b0c7bbd6a4a
Quebvihn <bamedinatorres@gmail.com>
2023-10-07 09:28:41 -0400

    variables

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ var key = "5ac110fde26370792fef708e7e50de6d"
 var cities = []
 var api = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
 
-var searchHistoryList = $('#search-history-list');
-var searchCityInput = $("#search-city");
-var searchCityButton = $("#search-city-button");
-var clearHistoryButton = $("#clear-history");
+var history = $('#search-history-list');
+var cityInput = $("#search-city");
+var cityButton = $("#search-city-button");
+var clearButton = $("#clear-history");
+
+var City = $("#current-city");
+var Temp = $("#current-temp");
+var Humidity = $("#current-humidity");
+var windSpeed = $("#current-wind-speed");
+var UVindex = $("#uv-index");
+
+var weatherContent = $("#weather-content");
+
+
+
+
